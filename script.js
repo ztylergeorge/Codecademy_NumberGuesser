e6a5fcca784614668fa436e9ba1494c3bb2631f5
ztylergeorge <ztylergeorge@gmail.com>
2021-08-27 15:51:14 -0400

    addition of compareGuesses

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ let currentRoundNumber = 1;
 //between 0 and 9
 
 function generateTarget() {
-    
+
     return Math.floor(Math.random() * 9);
 }
+
+//compareGuesses compares the user and computer guess
+//to the target
+
+function compareGuesses(user, computer, target) {
+
+    if ((Math.abs(target - user) < Math.abs(target - computer)) || (Math.abs(target - user) === Math.abs(target - computer))) {
+
+        return true;
+
+    }
+
+    else {
+
+        return false;
+    }
+
+}
+
