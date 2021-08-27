24c04b37847a218f390a30097d28453496ad73d1
ztylergeorge <ztylergeorge@gmail.com>
2021-08-27 15:35:58 -0400

    generateTarget() function made

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+//generateTarget() function returns random integer
+//between 0 and 9
+
+function generateTarget() {
+    
+    return Math.floor(Math.random() * 9);
+}
